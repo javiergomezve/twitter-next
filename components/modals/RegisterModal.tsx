3f61be2a17fe601c07c5d6d0ff583a95afe05a1a
Javier Gomez <javiergomezve@gmail.com>
2023-03-12 12:40:32 -0500

    [ADD] Register modal

---
+++ components/modals/RegisterModal.tsx
@@ -0,0 +1,77 @@
+import {useCallback, useState} from "react";
+import Input from "@/components/Input";
+import Modal from "@/components/Modal";
+import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegisterModal";
+
+
+const RegisterModal = () => {
+    const loginModal = useLoginModal()
+    const registerModal = useRegisterModal()
+
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onSubmit = useCallback(() => {
+        try {
+            setIsLoading(true)
+
+            // TODO: add register and login request
+
+            registerModal.onClose()
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }, [registerModal])
+
+    const bodyContent= (
+        <div className="flex flex-col gap-4">
+            <Input
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                disabled={isLoading}
+            />
+
+            <Input
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                disabled={isLoading}
+            />
+
+            <Input
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                disabled={isLoading}
+            />
+
+            <Input
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                disabled={isLoading}
+            />
+        </div>
+    )
+
+    return (
+        <Modal
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title="Create an account"
+            actionLabel="Register"
+            onClose={registerModal.onClose}
+            onSubmit={onSubmit}
+            body={bodyContent}
+        />
+    )
+}
+
+export default RegisterModal
